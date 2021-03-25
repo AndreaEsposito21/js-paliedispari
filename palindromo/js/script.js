0849d31a835ae2cfe8e61ba7228dc0f5a61f2807
Andrea Esposito <andexpox@gmail.com>
2021-03-25 15:32:42 +0100

    start

---
+++ palindromo/js/script.js
@@ -0,0 +1,9 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente di inserire una parola
+
+// Creo una funzione che deve capire se la parola inserita è palindroma
+//
+// 
